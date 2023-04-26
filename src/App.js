77405e3dffad3b993fa4d77dd9305ba294d37fa6
Amir-Toolabi-pro <amirooogamer81@gmail.com>
2023-04-26 18:23:05 +0430

    using  lodash.throttle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import throttle from 'lodash.throttle';
+import React, { useCallback, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [getNumber,setNumber] = useState(0);
+
+  
+  const loged = (getNumber)=>{
+    console.log(`number is : ${getNumber}`);
+  }
+  
+  const getThrottle = useCallback(throttle(loged,2000),[]);
+
+  const inc = ()=>{
+    setNumber(getNumber+1);
+    getThrottle(getNumber)
+  }
+  const dec = ()=>{
+    setNumber(getNumber-1);
+  }
+  return ( 
+    <>
+      <h3>تمرین throttle</h3>
+      <h1>{getNumber}</h1>
+      <button onClick={inc} >+</button>
+      <button onClick={dec} >-</button>
+    </>
+   );
 }
-
+ 
 export default App;
